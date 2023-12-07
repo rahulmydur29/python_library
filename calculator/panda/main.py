f72f7c717c8bd5d11e8bfe4768f3db86e5d7bb7a
dakshgupta127 <daksh.gupta@collaberadigital.com>
2023-12-07 17:04:21 +0530

    jkebkje

--- calculator/panda/main.py
+++ calculator/panda/main.py
@@ -1,3 +1,11 @@
+# Inside main.py
+
 def my_pandas_function():
-    # Your pandas-related function implementation here
-    pass
+    import pandas as pd
+    
+    # Sample pandas-related function implementation
+    data = {'Name': ['Alice', 'Bob', 'Charlie'],
+            'Age': [25, 30, 35]}
+    
+    df = pd.DataFrame(data)
+    print(df)
