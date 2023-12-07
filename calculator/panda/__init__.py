8fecbc68e41cd2086501c1d4b12a9b5083d38e93
dakshgupta127 <daksh.gupta@collaberadigital.com>
2023-12-07 15:47:13 +0530

    q

--- calculator/panda/__init__.py
+++ calculator/panda/__init__.py
@@ -1,12 +1,13 @@
 # my_library/__init__.py
 
-from .pde import MyPandasFunctions
+from .pde import my_function
 import pandas as pd
 
-class MyPandasLibrary:
-    @staticmethod
-    def conditional_usage(data):
-        if isinstance(data, pd.DataFrame):
-            MyPandasFunctions.my_function(data)
-        else:
-            print("Invalid input. Expected a pandas DataFrame.")
+# Conditional statement to use my_function
+def conditional_usage(data):
+    if isinstance(data, pd.DataFrame):
+        my_function(data)
+    else:
+        print("Invalid input. Expected a pandas DataFrame.")
+
+# Additional code in the __init__.py file if needed...
--- calculator/panda/pde.py
+++ calculator/panda/pde.py
@@ -1,13 +1,11 @@
-# my_library/pd.py
+# my_library/my_module.py
 
 import pandas as pd
 
-class MyPandasFunctions:
-    @staticmethod
-    def my_function(data):
-        if isinstance(data, pd.DataFrame):
-            # Perform some action on the DataFrame
-            # For example, print the first few rows
-            print(data.head())
-        else:
-            print("Input is not a pandas DataFrame")
+def my_function(data):
+    if isinstance(data, pd.DataFrame):
+        # Perform some action on the DataFrame
+        # For example, print the first few rows
+        print(data.head())
+    else:
+        print("Input is not a pandas DataFrame")
