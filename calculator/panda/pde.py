c073bd8b79edffe6d3e41d034ce294c6dc576d0d
dakshgupta127 <daksh.gupta@collaberadigital.com>
2023-12-07 15:40:49 +0530

    m

--- calculator/panda/pde.py
+++ calculator/panda/pde.py
@@ -1,11 +1,13 @@
-# my_library/my_module.py
+# my_library/pd.py
 
 import pandas as pd
 
-def my_function(data):
-    if isinstance(data, pd.DataFrame):
-        # Perform some action on the DataFrame
-        # For example, print the first few rows
-        print(data.head())
-    else:
-        print("Input is not a pandas DataFrame")
+class MyPandasFunctions:
+    @staticmethod
+    def my_function(data):
+        if isinstance(data, pd.DataFrame):
+            # Perform some action on the DataFrame
+            # For example, print the first few rows
+            print(data.head())
+        else:
+            print("Input is not a pandas DataFrame")
