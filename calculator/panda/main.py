080b5df909e0f1615bb4b4177d79c61739b03801
dakshgupta127 <daksh.gupta@collaberadigital.com>
2023-12-07 17:09:40 +0530

    njsk

--- calculator/panda/main.py
+++ calculator/panda/main.py
@@ -1,11 +1,13 @@
 # Inside main.py
 
-def my_pandas_function():
-    import pandas as pd
-    
-    # Sample pandas-related function implementation
-    data = {'Name': ['Alice', 'Bob', 'Charlie'],
-            'Age': [25, 30, 35]}
-    
-    df = pd.DataFrame(data)
-    print(df)
+import pandas as pd
+
+class PandasFunctionality:
+    @staticmethod
+    def my_pandas_function():
+        # Sample pandas-related function implementation
+        data = {'Name': ['Alice', 'Bob', 'Charlie'],
+                'Age': [25, 30, 35]}
+        
+        df = pd.DataFrame(data)
+        print(df)
