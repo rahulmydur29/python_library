2618b19aaa0eab20a20c7115ba6d71e7b372ffdc
rahulmydur29 <rahul.mydur@collaberadigital.com>
2023-12-07 17:27:03 +0530

    pdd commmit

--- calculator/panda/main.py
+++ calculator/panda/main.py
@@ -1,13 +1,11 @@
-# Inside main.py
+# main.py
 
 import pandas as pd
 
-class PandasFunctionality:
-    @staticmethod
-    def my_pandas_function():
-        # Sample pandas-related function implementation
-        data = {'Name': ['Alice', 'Bob', 'Charlie'],
-                'Age': [25, 30, 35]}
-        
-        df = pd.DataFrame(data)
-        print(df)
+def my_pandas_function():
+    # Sample pandas-related function implementation
+    data = {'Name': ['Alice', 'Bob', 'Charlie'],
+            'Age': [25, 30, 35]}
+    
+    df = pd.DataFrame(data)
+    print(df)
